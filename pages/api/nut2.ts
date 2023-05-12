562b0725a5eff4857933ea76f00841304aa5100c
신지훈 <rec8730@gmail.com>
2023-05-12 16:22:19 +0900

    mongoDB 이름 변경

--- pages/api/nut2.ts
+++ pages/api/nut2.ts
@@ -1,19 +1,14 @@
 import clientPromise from "../../lib/mongodb";
 import type { NextApiResponse, NextApiRequest } from "next";
 
-
-
-
 export default async function Movies(req:NextApiRequest, res:NextApiResponse) {
 try {
     const client = await clientPromise;
     const db = client.db("nut2");
 
     const nut2Post = await db
-        .collection("post")
+        .collection("models")
         .find({})
-        .sort({ metacritic: -1 })
-        .limit(10)
         .toArray();
     res.json(nut2Post);
 } catch (e) {
