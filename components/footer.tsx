593ebdb7b958dda1dffb3e7469c81e8234776fb3
신지훈 <rec8730@gmail.com>
2023-05-16 18:20:10 +0900

    footer 레이아웃 추가

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default function Footer() {
+  return <FooterContainer>footer</FooterContainer>;
+}
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  height: 200px;
+  padding: 32px 230px;
+  background-color: #252525;
+  border-top: 1px solid #eeeeee;
+  background-color: #ffffff;
+`;
