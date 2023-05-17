3bfcba93e3eccd985771b567cb9fcc55ad1f1db2
신지훈 <rec8730@gmail.com>
2023-05-17 16:44:41 +0900

    navbar 프로필 상태 추가

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,85 @@
 import { useRouter } from "next/router";
+import { useSession, signOut } from "next-auth/react";
 
 // MUI
 import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
 
 import styled from "styled-components";
+
+export default function Navbar() {
+  const router = useRouter();
+
+  const { data: session } = useSession();
+
+  const handleGoPage = (pages: string) => {
+    router.push(pages);
+  };
+  return (
+    <NavbarContainer>
+      <Left>
+        <Logo>Nut2</Logo>
+
+        <Lists>
+          <Button
+            onClick={() => {
+              handleGoPage("/");
+            }}
+          >
+            홈
+          </Button>
+          <Button
+            onClick={() => {
+              handleGoPage("/list");
+            }}
+          >
+            모델
+          </Button>
+          <Button
+            onClick={() => {
+              handleGoPage("/");
+            }}
+          >
+            스타일
+          </Button>
+        </Lists>
+      </Left>
+      <LoginBox>
+        {session ? (
+          <Profiles>
+            <NameTitle>{session?.user?.name}</NameTitle>
+            <Avatar
+              alt="Profile"
+              src={`${session?.user?.image}`}
+              sx={{ width: 30, height: 30 }}
+            />
+          </Profiles>
+        ) : (
+          ""
+        )}
+
+        {session ? (
+          <Button
+            onClick={() => {
+              signOut();
+            }}
+          >
+            로그아웃
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            onClick={() => {
+              handleGoPage("/login");
+            }}
+          >
+            로그인
+          </Button>
+        )}
+      </LoginBox>
+    </NavbarContainer>
+  );
+}
 
 const NavbarContainer = styled.div`
   display: flex;
@@ -31,52 +107,17 @@ const Lists = styled.div`
   gap: 20px;
 `;
 
-const LoginBox = styled.div``;
+const LoginBox = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 
-export default function Navbar() {
-  const router = useRouter();
+const Profiles = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
 
-  const handleGoPage = (pages: string) => {
-    router.push(pages);
-  };
-  return (
-    <NavbarContainer>
-      <Left>
-        <Logo>Nut2</Logo>
-        <Lists>
-          <Button
-            onClick={() => {
-              handleGoPage("/");
-            }}
-          >
-            홈
-          </Button>
-          <Button
-            onClick={() => {
-              handleGoPage("/list");
-            }}
-          >
-            모델
-          </Button>
-          <Button
-            onClick={() => {
-              handleGoPage("/");
-            }}
-          >
-            스타일
-          </Button>
-        </Lists>
-      </Left>
-      <LoginBox>
-        <Button
-          variant="contained"
-          onClick={() => {
-            handleGoPage("/login");
-          }}
-        >
-          Login
-        </Button>
-      </LoginBox>
-    </NavbarContainer>
-  );
-}
+const NameTitle = styled.p`
+  font-size: 14px;
+`;
