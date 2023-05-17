4b80590b40c6db38d30542bf7607b0d2c0208d92
신지훈 <rec8730@gmail.com>
2023-05-17 16:46:25 +0900

    스타일 페이지 카드 형식 디자인 변경

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -8,6 +8,20 @@ import styled from "styled-components";
 //Mui
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 interface listType {
   userName: string;
@@ -17,6 +31,7 @@ interface listType {
   userId: string;
   fashionShop: { label: string; url: string };
   _id: string;
+  avatar: string;
 }
 
 export default function List({
@@ -41,18 +56,51 @@ export default function List({
         />
       </ModelTextFieldBox>
       <ModelCardList>
-        {list.map(({ userAge, snsId, _id, fashionShop }: listType) => {
+        {list.map(({ _id, avatar, snsId }: listType) => {
           return (
-            <ModelCard
-              key={_id}
-              onClick={() => {
-                router.push(`detail/${_id}`);
-              }}
-            >
-              <h4>@ {snsId}</h4>
-              <p>{userAge}</p>
-              <p>{fashionShop.label}</p>
-            </ModelCard>
+            <>
+              <Card sx={{ maxWidth: "320px" }}>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      sx={{ bgcolor: red[500] }}
+                      aria-label="recipe"
+                      src={`${avatar}`}
+                    >
+                      R
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title={`@ ${snsId}`}
+                  subheader="September 14, 2016"
+                />
+                <CardMedia
+                  component="img"
+                  height=""
+                  image="./images/fastion0.jpg"
+                  alt="Fashion Images"
+                  onClick={() => {
+                    router.push(`/detail/${_id}`);
+                  }}
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    This impressive paella is a perfect party dish and a fun
+                    meal to cook together with your guests. Add 1 cup of frozen
+                    peas along with the mussels, if you like.
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </>
           );
         })}
       </ModelCardList>
