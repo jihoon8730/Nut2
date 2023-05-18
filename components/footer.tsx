1908e5b465c283122e49dd2d8c1583d73b0f1155
신지훈 <rec8730@gmail.com>
2023-05-18 23:49:32 +0900

    footer 레이아웃

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import styled from "styled-components";
 
 export default function Footer() {
-  return <FooterContainer>footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <FooterInfoBox>
+        <Title>Nut2</Title>
+        <GoLinks>
+          <GoLink href="https://github.com/jihoon8730">Github</GoLink>
+          <GoLink href="https://github.com/jihoon8730">Email</GoLink>
+          <GoLink href="https://rec8730.tistory.com/">Blog</GoLink>
+          <GoLink href="https://www.instagram.com/0112mm_nin/">SNS</GoLink>
+        </GoLinks>
+      </FooterInfoBox>
+      <Description>제작자 : Shin ji hoon</Description>
+    </FooterContainer>
+  );
 }
 
 const FooterContainer = styled.footer`
@@ -11,4 +25,31 @@ const FooterContainer = styled.footer`
   background-color: #252525;
   border-top: 1px solid #eeeeee;
   background-color: #ffffff;
+`;
+
+const FooterInfoBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 200px;
+  border-bottom: 1px solid #eeeeee;
+  padding: 20px 0px;
+`;
+
+const Title = styled.h4`
+  font-size: 32px;
+`;
+
+const GoLinks = styled.div`
+  display: flex;
+  gap: 10rem;
+  width: 100%;
+`;
+
+const GoLink = styled(Link)`
+  color: gray;
+`;
+
+const Description = styled.h4`
+  margin-top: 16px;
+  font-size: 14px;
 `;
